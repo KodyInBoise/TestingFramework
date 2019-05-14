1f62023131d1d82ca5f289653851754aeba4dd6e
Kody-PC\Kody <kody.kriner@gmail.com>
2019-05-14 17:18:25 -0600

    Added objects for UserInfos and Roles to start doing permissions based features

---
+++ Extensions/RoleHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestingFramework.Data;
+using TestingFramework.Models;
+
+namespace TestingFramework.Extensions
+{
+    public enum RoleType : int
+    {
+        Admin = 0,
+        PowerUser = 1,
+        User = 2
+    }
+
+
+    public class RoleHelper
+    {
+        public static void SeedAppRoles(ApplicationDbContext database)
+        {
+            var roles = database.Roles.ToList();
+
+            if (roles.FirstOrDefault(r => r.Type == RoleType.Admin) == null)
+            {
+                database.Roles.Add(new RoleModel
+                {
+                    Type = RoleType.Admin,
+                    Name = "Admin",
+                    Description = "Application Administrator"
+                });
+            }
+
+            if (roles.FirstOrDefault(r => r.Type == RoleType.PowerUser) == null)
+            {
+                database.Roles.Add(new RoleModel
+                {
+                    Type = RoleType.PowerUser,
+                    Name = "Power User",
+                    Description = "Application user with setup capabilities"
+                });
+            }
+
+            if (roles.FirstOrDefault(r => r.Type == RoleType.User) == null)
+            {
+                database.Roles.Add(new RoleModel
+                {
+                    Type = RoleType.User,
+                    Name = "User",
+                    Description = "Application user who can participate in testing and view results"
+                });
+            }
+
+            database.SaveChanges();
+        }
+    }
+}
---
+++ Models/RoleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestingFramework.Extensions;
+
+namespace TestingFramework.Models
+{
+    public class RoleModel
+    {
+        public Guid ID { get; set; }
+        public RoleType Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
